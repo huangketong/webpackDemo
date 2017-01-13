48da48a74e04bb81885564741a734e774e12b37e
[huangketong] <huangkt@joyowo.com>
2017-01-13 17:45:10 +0800

    dataAjax

---
+++ src/js/dataAjax/index.jsx
@@ -0,0 +1,55 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+
+var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  loadCommentsFromServer: function(){
+      $.ajax({
+          url: this.props.url,
+          dataType: 'json',
+          cache: false,
+          success: function(data){
+              this.setState({data: data});
+          }.bind(this),
+          error: function(xhr, status, err){
+              console.error(this.props.url, status, err.toString());
+          }.bind(this)
+      });
+  },
+  commentDidMount: function(){
+      this.loadCommentsFromServer();
+      setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.state.data} />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+
+
+
+// class CommentBox extends React.Compent{
+//     getInitialState : function(){
+//         return{data:[]};
+//     },
+//     render: function(){
+//         return(
+//             <div className="commentBox">
+//                 <h1>Comments</h1>
+//                 <CommentList data={this.props.data} />
+//                 <CommentForm />
+//             </div>
+//         );
+//     }
+// }
+
+
+ReactDom.render( <CommentBox url="/api/comments"/> , document.getElementById('root'));
